a7706bb4a4ecd6c035b15865f11f09fa5a1131c1
Crea <secret@secret.secret>
2021-06-29 12:31:54 +0200

    Distance Display
    Contains:
    - Distance display + dummy implementation
    - Fixed camera bug

--- Assets/Scripts/Audio/AudioPlayer.cs
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -1,203 +1,217 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioPlayer : MonoBehaviour
 {
-    [SerializeField]
-    private Transform target;
-    [Header("Repetitions")]
-    [SerializeField]
-    private List<Interval> intervals_repetitions;
-    /*
+	#region Variables
+	[SerializeField]
+	private Transform target;
+	[Header("Repetitions")]
+	[SerializeField]
+	private List<Interval> intervals_repetitions;
+	/*
     [SerializeField]
     private float repetitions_per_second_start = 1;
     [SerializeField]
     private float repetitions_per_second_max = 20;    
     */
-    [Header("Volume")]
-    /*
+	[Header("Volume")]
+	/*
     [SerializeField]
     private float volume_start = 0.05f;
     */
-    [SerializeField]
-    private float volume_max = 1;
-    [SerializeField]
-    private List<Interval> intervals_volume;
+	[SerializeField]
+	private float volume_max = 1;
+	[SerializeField]
+	private List<Interval> intervals_volume;
 
+	//public bool playAudio = true;
+	[Header("Tools")]
+	public bool volume_mode = true;
+	[SerializeField]
+	[Tooltip("Defines distance intervals as color key locations.")]
+	private Gradient intervals_percentages;
 
-    //public bool playAudio = true;
-    [Header("Tools")]
-    public bool volume_mode = true;
-    [SerializeField]
-    [Tooltip("Defines distance intervals as color key locations.")]
-    private Gradient intervals_percentages;
+	private AudioSource source;
 
-    private AudioSource source;
+	private float starting_dist;
+	private float repetitions_per_second_curr;
+	private float volume_curr;
 
-    private float starting_dist;
-    private float repetitions_per_second_curr;
-    private float volume_curr;
+	private bool is_playing;
+	private bool mode_prev;
+	private bool mode_curr;
 
-    private bool is_playing;
-    private bool mode_prev;
-    private bool mode_curr;
+	//Reminder: TODO maybe create an own class for text (or rename class)
+	[SerializeField]
+	private GameObject distanceDisplay;
+	[SerializeField]
+	private Text distanceText;
+	public bool isUsingText = false; //TODO only testcode
+	#endregion
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	private void OnEnable()
+	{
+		source = GetComponent<AudioSource>();
+		volume_curr = volume_max;
 
-    }
+		starting_dist = UpdateDistance();
+		MatchPercentages();
 
-    private void OnEnable()
-    {
-        source = GetComponent<AudioSource>();
-        volume_curr = volume_max;
+		repetitions_per_second_curr = 3f;
 
-        starting_dist = UpdateDistance();
-        MatchPercentages();
+		mode_prev = volume_mode;
+	}
 
-        repetitions_per_second_curr = 3f;
+	private void Update()
+	{
+		distanceDisplay.SetActive(isUsingText);//TODO I should find a better solution
+	}
 
-        mode_prev = volume_mode;
+	void FixedUpdate()
+	{
+		mode_curr = volume_mode || isUsingText;
+		ChooseMode();
+	}
 
-    }
+	void MatchPercentages()
+	{
+		// maybe catch this
+		int id = 0;
+		foreach (GradientColorKey key in intervals_percentages.colorKeys)
+		{
+			intervals_repetitions[id].SetPercentage(key.time);
+			intervals_volume[id].SetPercentage(key.time);
+			id++;
+		}
+	}
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        mode_curr = volume_mode;
-        ChooseMode();
-    }
+	Interval SelectInterval(List<Interval> from, float percentage)
+	{
+		Interval prev_interval = from[0];
+		foreach (Interval interval in from)
+		{
+			float curr_percentage = interval.GetPercentage();
+			if (percentage > curr_percentage) prev_interval = interval;
+			else
+			{
+				if (percentage == curr_percentage) return interval;
+				else return prev_interval;
+			}
+		}
+		return from[from.Count - 1];
+	}
 
-    void MatchPercentages()
-    {
-        // maybe catch this
-        int id = 0;
-        foreach (GradientColorKey key in intervals_percentages.colorKeys)
-        {
-            intervals_repetitions[id].SetPercentage(key.time);
-            intervals_volume[id].SetPercentage(key.time);
-            id++;
-        }
-    }
+	float CalculateLerpPercentage(List<Interval> from, Interval current_interval, float dist_percentage)
+	{
+		Interval next_interval = from[from.Count - 1];
+		int id = 0;
+		int next_id = from.Count - 1;
+		foreach (Interval interval in from)
+		{
+			if (interval.GetPercentage() == current_interval.GetPercentage())
+			{
+				next_id = (id < from.Count - 1) ? id + 1 : from.Count - 1;
+				next_interval = from[next_id];
+				break;
+			}
+			id++;
+		}
 
-    Interval SelectInterval(List<Interval> from, float percentage)
-    {
-        Interval prev_interval = from[0];
-        foreach(Interval interval in from)
-        {
-            float curr_percentage = interval.GetPercentage();
-            if (percentage > curr_percentage) prev_interval = interval;
-            else
-            {
-                if (percentage == curr_percentage) return interval;
-                else return prev_interval;
-            }
-        }
-        return from[from.Count - 1];
-    }
+		//we lerp between current interval percentage (as marked in gradient) to the next interval key or 1 for last key
+		float upper_value = (next_id != from.Count - 1) ? next_interval.GetPercentage() : 1f;
+		float lerp_percentage = Mathf.InverseLerp(current_interval.GetPercentage(), upper_value, dist_percentage);
+		//Debug.Log(lerp_percentage + " " + current_interval.lower + " " + current_interval.upper);
+		//Debug.Log(current_interval.GetPercentage() + " " + upper_value);
 
-    float CalculateLerpPercentage(List<Interval> from, Interval current_interval, float dist_percentage)
-    {
-        Interval next_interval = from[from.Count - 1];
-        int id = 0;
-        int next_id = from.Count - 1;
-        foreach (Interval interval in from)
-        {
-            if (interval.GetPercentage() == current_interval.GetPercentage())
-            {
-                next_id = (id < from.Count - 1) ? id + 1 : from.Count - 1;
-                next_interval = from[next_id];
-                break;
-            }
-            id++;
-        }
+		return lerp_percentage;
+	}
 
-        //we lerp between current interval percentage (as marked in gradient) to the next interval key or 1 for last key
-        float upper_value = (next_id != from.Count - 1) ? next_interval.GetPercentage() : 1f;
-        float lerp_percentage = Mathf.InverseLerp(current_interval.GetPercentage(), upper_value, dist_percentage);
-        //Debug.Log(lerp_percentage + " " + current_interval.lower + " " + current_interval.upper);
-        //Debug.Log(current_interval.GetPercentage() + " " + upper_value);
+	void ChooseMode()
+	{
+		if (mode_curr != mode_prev)
+		{
+			if (mode_curr)
+			{
+				StopCoroutine(OnPlayRepetition());
+				repetitions_per_second_curr = 1f;
+				VolumeStep();
+			}
+			else
+			{
+				StopCoroutine(OnPlayRepetition());
+				source.volume = volume_max;
+				RepetitionStep();
+			}
+			mode_prev = mode_curr;
+		}
+		else
+		{
+			if (mode_curr) VolumeStep();
+			else RepetitionStep();
+		}
+	}
 
-        return lerp_percentage;
-    }
+	void RepetitionStep()
+	{
+		AdaptRepetitions();
+		StartCoroutine(OnPlayRepetition());
+	}
 
-    void ChooseMode()
-    {
-        if(mode_curr != mode_prev)
-        {
-            if (mode_curr)
-            {
-                StopCoroutine(OnPlayRepetition());
-                repetitions_per_second_curr = 1f;
-                VolumeStep();
-            }
-            else
-            {
-                StopCoroutine(OnPlayRepetition());
-                source.volume = volume_max;
-                RepetitionStep();
-            }
-            mode_prev = mode_curr;
-        }
-        else
-        {
-            if (mode_curr) VolumeStep();
-            else RepetitionStep();
-        }
-    }
+	void VolumeStep()
+	{
+		AdaptVolume();
+		StartCoroutine(OnPlayRepetition());
+	}
 
-    void RepetitionStep()
-    {
-        AdaptRepetitions();
-        StartCoroutine(OnPlayRepetition());
-    }
+	private void AdaptRepetitions()
+	{
+		float current_dist = UpdateDistance();
+		//Debug.Log("Current distance " + current_dist);
 
-    void VolumeStep()
-    {
-        AdaptVolume();
-        StartCoroutine(OnPlayRepetition());
-    }
+		float dist_percentage = 1f - Mathf.Clamp(current_dist / starting_dist, 0f, 1f);
+		//Debug.Log("Current distance % " + dist_percentage);
+		Interval current_interval = SelectInterval(intervals_repetitions, dist_percentage);
 
-    private void AdaptRepetitions()
-    {
-        float current_dist = UpdateDistance();
-        //Debug.Log("Current distance " + current_dist);
+		float lerp_percentage = CalculateLerpPercentage(intervals_repetitions, current_interval, dist_percentage);
+		repetitions_per_second_curr = Mathf.Lerp(current_interval.lower, current_interval.upper, lerp_percentage);
+	}
 
-        float dist_percentage = 1f - Mathf.Clamp(current_dist / starting_dist, 0f, 1f);
-        //Debug.Log("Current distance % " + dist_percentage);
-        Interval current_interval = SelectInterval(intervals_repetitions, dist_percentage);
+	void AdaptVolume()
+	{
+		float current_dist = UpdateDistance();
+		float dist_percentage = 1f - Mathf.Clamp(current_dist / starting_dist, 0f, 1f);
 
-        float lerp_percentage = CalculateLerpPercentage(intervals_repetitions, current_interval, dist_percentage);
-        repetitions_per_second_curr = Mathf.Lerp(current_interval.lower, current_interval.upper, lerp_percentage);
-    }
+		Interval current_interval = SelectInterval(intervals_volume, dist_percentage);
 
-    void AdaptVolume()
-    {
-        float current_dist = UpdateDistance();
-        float dist_percentage = 1f - Mathf.Clamp(current_dist / starting_dist, 0f, 1f);
+		float lerp_percentage = CalculateLerpPercentage(intervals_volume, current_interval, dist_percentage);
+		source.volume = Mathf.Lerp(current_interval.lower, current_interval.upper, lerp_percentage);
+		//Debug.Log("Vol " + source.volume + " lerp_percentage " + lerp_percentage);
+	}
 
-        Interval current_interval = SelectInterval(intervals_volume, dist_percentage);
+	private float UpdateDistance()
+	{
+		return Mathf.Abs((transform.parent.position - target.transform.position).magnitude);
+	}
 
-        float lerp_percentage = CalculateLerpPercentage(intervals_volume, current_interval, dist_percentage);
-        source.volume = Mathf.Lerp(current_interval.lower, current_interval.upper, lerp_percentage);
-        //Debug.Log("Vol " + source.volume + " lerp_percentage " + lerp_percentage);
-    }
+	private IEnumerator OnPlayRepetition()
+	{
+		if (!is_playing)
+		{
+			is_playing = true;
+			if (isUsingText)
+			{
+				distanceText.text = string.Format("{0:00}m", UpdateDistance());
+			}
+			else
+			{
+				source.Play();
+			}
 
-    private float UpdateDistance()
-    {
-        return Mathf.Abs((transform.parent.position - target.transform.position).magnitude);
-    }
-
-    private IEnumerator OnPlayRepetition()
-    {
-        if (!is_playing)
-        {
-            is_playing = true;
-            source.Play();
-            yield return new WaitForSeconds(1 / repetitions_per_second_curr);
-            is_playing = false;
-        }
-    }
+			yield return new WaitForSeconds(1 / repetitions_per_second_curr);
+			is_playing = false;
+		}
+	}
 }
--- Assets/Scripts/FirstPersonController/Controller/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController/Controller/FirstPersonController.cs
@@ -10,147 +10,154 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class FirstPersonController : MonoBehaviour, ICharacterSignals
 {
+	#region Character Signals
 
-    #region Character Signals
+	public IObservable<Vector3> Moved => _moved;
+	private Subject<Vector3> _moved;
 
-    public IObservable<Vector3> Moved => _moved;
-    private Subject<Vector3> _moved;
+	public ReactiveProperty<bool> IsRunning => _isRunning;
+	private ReactiveProperty<bool> _isRunning;
 
-    public ReactiveProperty<bool> IsRunning => _isRunning;
-    private ReactiveProperty<bool> _isRunning;
+	public IObservable<Unit> Landed => _landed;
+	private Subject<Unit> _landed;
 
-    public IObservable<Unit> Landed => _landed;
-    private Subject<Unit> _landed;
+	public IObservable<Unit> Jumped => _jumped;
+	private Subject<Unit> _jumped;
 
-    public IObservable<Unit> Jumped => _jumped;
-    private Subject<Unit> _jumped;
+	public IObservable<Unit> Stepped => _stepped;
+	private Subject<Unit> _stepped;
 
-    public IObservable<Unit> Stepped => _stepped;
-    private Subject<Unit> _stepped;
+	#endregion
 
-    #endregion
+	#region Variables
+	[Header("References")]
+	[SerializeField] private FirstPersonControllerInput firstPersonControllerInput;
+	private CharacterController _characterController;
+	private Camera _camera;
+	private float walkingSpeed = 5f;
+	private float runSpeed = 10f;
+	private float jumpSpeed = 5f;
+	[SerializeField] private float strideLength = 4f;
+	public float StrideLength => strideLength;
 
+	private float stickToGround = 5.0f;
 
+	[Range(0, 80)] [SerializeField] private float minVTopAngle = 20f;
+	[Range(0, 80)] [SerializeField] private float minVBottomAngle = 30f;
+	#endregion
 
-    [Header("References")]
-    [SerializeField] private FirstPersonControllerInput firstPersonControllerInput;
-    private CharacterController _characterController;
-    private Camera _camera;
-    private float walkingSpeed = 5f;
-    private float runSpeed = 10f;
-    private float jumpSpeed = 5f;
-    [SerializeField] private float strideLength = 4f;
-    public float StrideLength => strideLength;
+	public struct MoveInputData
+	{
+		public readonly Vector2 move;
+		public readonly bool jump;
+		public MoveInputData(Vector2 move, bool jump)
+		{
+			this.move = move;
+			this.jump = jump;
+		}
+	}
 
-    private float stickToGround = 5.0f;
+	private void Awake()
+	{
+		_characterController = GetComponent<CharacterController>();
+		_camera = GetComponentInChildren<Camera>();
 
-    [Range(-90, 0)] [SerializeField] private float minViewAngle = -60f;
-    [Range(0, 90)] [SerializeField] private float maxViewAngle = 60f;
+		_isRunning = new ReactiveProperty<bool>(false);
+		_moved = new Subject<Vector3>().AddTo(this);
+	}
 
+	private void Start()
+	{
+		_characterController.Move(-stickToGround * transform.up);
 
-    public struct MoveInputData
-    {
-        public readonly Vector2 move;
-        public readonly bool jump;
-        public MoveInputData(Vector2 move, bool jump)
-        {
-            this.move = move;
-            this.jump = jump;
-        }
-    }
+		MoveHandler();
+		LookHandler();
+	}
 
-    private void Awake() {
-        _characterController = GetComponent<CharacterController>();
-        _camera = GetComponentInChildren<Camera>();
+	private void MoveHandler()
+	{
+		bool wasGrounded = _characterController.isGrounded;
 
-        _isRunning = new ReactiveProperty<bool>(false);
-        _moved = new Subject<Vector3>().AddTo(this);
-    }
+		var jumpLatch = LatchObservables.Latch(this.UpdateAsObservable(), firstPersonControllerInput.Jump, false);
+		firstPersonControllerInput.Move
+			.Zip(jumpLatch, (m, j) => new MoveInputData(m, j))
+			.Where(moveInputData => moveInputData.jump || moveInputData.move != Vector2.zero)
+			.Subscribe(input =>
+			{
+				float speed = firstPersonControllerInput.Run.Value ? runSpeed : walkingSpeed;
 
-    private void Start()
-    {
-        _characterController.Move(-stickToGround * transform.up);
+				float verticalVelocity = 0f;
+				if (input.jump && wasGrounded)
+				{// We're on the ground and want to jump.
+					verticalVelocity = jumpSpeed;
+				}
+				else if (!wasGrounded)
+				{// We're in the air: apply gravity.
+					verticalVelocity = _characterController.velocity.y + (Physics.gravity.y * Time.deltaTime * 3.0f);
+				}
+				else
+				{
+					// We're on the ground: push us down a little.
+					// (Required for character.isGrounded to work.)
+					verticalVelocity = -Mathf.Abs(stickToGround);
+				}
 
-        MoveHandler();
-        LookHandler();
+				Vector2 horizontalVelocity = input.move * speed;
+				Vector3 characterVelocity = transform.TransformVector(new Vector3(
+					horizontalVelocity.x,
+					verticalVelocity,
+					horizontalVelocity.y));
+				Vector3 distance = characterVelocity * Time.deltaTime;
+				_characterController.Move(distance);
 
-        
-    }
+				//Update Values
+				bool tempIsRunning = false;
+				if (wasGrounded && _characterController.isGrounded)
+				{
+					// Both started and ended this frame on the ground.
+					_moved.OnNext(_characterController.velocity * Time.deltaTime);
+					if (_characterController.velocity.magnitude > 0)
+					{
+						// The chaarcter is running if the input is active and the character is actually moving on the ground
+						tempIsRunning = firstPersonControllerInput.Run.Value;
+					}
+				}
+				_isRunning.Value = tempIsRunning;
 
-    private void MoveHandler()
-    {
+			}).AddTo(this);
+	}
 
-        var wasGrounded = _characterController.isGrounded;
+	private void LookHandler()
+	{
+		firstPersonControllerInput.Look
+					.Where(v => v != Vector2.zero)
+					.Subscribe(rotation =>
+					{
+						float horizontal = rotation.x * Time.deltaTime;//TODO multiply mouse sensitivity value
+						float vertical = -rotation.y * Time.deltaTime;
 
-        var jumpLatch = LatchObservables.Latch(this.UpdateAsObservable(), firstPersonControllerInput.Jump, false);
-                firstPersonControllerInput.Move
-                    .Zip(jumpLatch, (m, j) => new MoveInputData(m,j))
-                    .Where(moveInputData => moveInputData.jump ||
-                                            moveInputData.move != Vector2.zero)
-                    .Subscribe(input => {
-                        var speed = firstPersonControllerInput.Run.Value ? runSpeed : walkingSpeed;
+						Vector3 old = _camera.transform.localRotation.eulerAngles;
+						//Down = 90, forward = 0/360, Up = 270
+						float dest = old.x + vertical;
+						if (dest > 180)
+						{
+							float upperBorder = 270 + minVTopAngle;
+							if (dest < upperBorder)
+							{
+								dest = upperBorder;
+							}
+						}
+						else
+						{
+							float lowerBorder = 90 - minVBottomAngle;
+							if (dest > lowerBorder)
+							{
+								dest = lowerBorder;
+							}
+						}
+						_camera.transform.localRotation = Quaternion.Euler(dest, old.y, 0);
+						transform.rotation *= Quaternion.Euler(0, horizontal, 0);
 
-                        var verticalVelocity = 0f;
-                        if (input.jump && wasGrounded)
-                        {
-                            // We're on the ground and want to jump.
-                            verticalVelocity = jumpSpeed;
-                        }
-                        else if (!wasGrounded)
-                        {
-                            // We're in the air: apply gravity.
-                            verticalVelocity = _characterController.velocity.y +
-                            (Physics.gravity.y * Time.deltaTime * 3.0f);
-                        }
-                        else
-                        {
-                            // We're on the ground: push us down a little.
-                            // (Required for character.isGrounded to work.)
-                            verticalVelocity = -Mathf.Abs(stickToGround);
-                        }
-
-                        var horizontalVelocity = input.move * speed;
-                        var characterVelocity = transform.TransformVector(new Vector3(
-                            horizontalVelocity.x,
-                            verticalVelocity,
-                            horizontalVelocity.y));
-                        var distance = characterVelocity * Time.deltaTime;
-                        _characterController.Move(distance);
-
-                        //Update Values
-                        var tempIsRunning = false;
-                        if (wasGrounded && _characterController.isGrounded)
-                        {
-                            // Both started and ended this frame on the ground.
-                            _moved.OnNext(_characterController.velocity * Time.deltaTime);
-                            if (_characterController.velocity.magnitude > 0)
-                            {
-                                // The chaarcter is running if the input is active and the character is actually moving on the ground
-                                tempIsRunning = firstPersonControllerInput.Run.Value;
-                            }
-                        }
-                        _isRunning.Value = tempIsRunning;
-
-                    }).AddTo(this);
-    }
-
-    private void LookHandler()
-    {
-        firstPersonControllerInput.Look
-                    .Where(v => v != Vector2.zero)
-                    .Subscribe(input =>
-                    {
-                        var horizontalLook = input.x * Vector3.up * Time.deltaTime;
-                        transform.localRotation *= Quaternion.Euler(horizontalLook);
-
-                        var verticalLook = input.y * Vector3.left * Time.deltaTime;
-
-                        var newQ = _camera.transform.localRotation * Quaternion.Euler(verticalLook);
-                        _camera.transform.localRotation =
-                                RotationTools.ClampRotationAroundXAxis(newQ, -maxViewAngle, -minViewAngle);
-
-                    }).AddTo(this);
-    }
+					}).AddTo(this);
+	}
 }
-
-
